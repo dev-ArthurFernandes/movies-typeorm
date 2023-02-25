d02aead7cf3bba4c9f6bdf11e327a77dcd84ffe9
Arthur <arthurdesosuafernandes@gmail.com>
2023-02-25 13:30:48 -0300

    feat: Create Post Route

--- src/controller/movies.controller.ts
+++ src/controller/movies.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response  } from "express";
+import { IMoviesRequest, IMoviesResult } from "../interfaces";
+import { createMovieService } from "../services";
 
 const createMovieController = async (req: Request, res: Response): Promise<Response> => {
 
-    
+    const movieData: IMoviesRequest = req.body
 
-    return res.json()
+    const newMovie: IMoviesResult = await createMovieService(movieData)
+
+    return res.status(201).json(newMovie)
 }
 
 export default createMovieController
